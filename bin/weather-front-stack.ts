84fd012f0827f6fbec8db8c36f5eaf10b4a258f3
Mikko Kotola <mikko.kotola@iki.fi>
2024-03-16 23:44:10 +0200

    Initial AWS stack for weatherfront

--- bin/weather-front-stack.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { WeatherFrontStackStack } from '../lib/weather-front-stack-stack';
-
-const app = new cdk.App();
-new WeatherFrontStackStack(app, 'WeatherFrontStackStack');
